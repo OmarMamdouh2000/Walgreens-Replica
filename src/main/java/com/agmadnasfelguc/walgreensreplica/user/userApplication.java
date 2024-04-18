15fb305a6b69874246a9c59db2661b5770b763a1
Omar_Mamdouh <omar.mamdouh@techsa.io>
2024-04-18 18:19:16 +0200

    Basic Structure for User Management

---
+++ src/main/java/com/agmadnasfelguc/walgreensreplica/user/userApplication.java
@@ -0,0 +1,13 @@
+package com.agmadnasfelguc.walgreensreplica.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class userApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(userApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agmadnasfelguc/walgreensreplica/user/userApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agmadnasfelguc.walgreensreplica.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class userApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
