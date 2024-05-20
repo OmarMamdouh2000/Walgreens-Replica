1540598074cf5d473d855aeb0d0c8943a7f140ea
ZiadHegazy99 <ziad.hegazy@student.guc.edu.eg>
2024-05-20 20:43:59 +0300

    aa

--- src/main/java/com/example/Cache/RedisConfig.java
+++ src/main/java/com/example/Cache/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.Cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
